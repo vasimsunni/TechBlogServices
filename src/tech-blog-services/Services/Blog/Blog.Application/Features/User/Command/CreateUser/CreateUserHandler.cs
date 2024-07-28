4776c84918ff0f2712f1f942cad0404c78de3dac
unknown <sunni.vasim@gmail.com>
2024-07-28 20:43:46 +0400

    Improvement in CreateUserHandler

--- src/tech-blog-services/Services/Blog/Blog.Application/Features/User/Command/CreateUser/CreateUserHandler.cs
+++ src/tech-blog-services/Services/Blog/Blog.Application/Features/User/Command/CreateUser/CreateUserHandler.cs
@@ -1,4 +1,5 @@
 ﻿using BuildingBlocks.Encryption;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Blog.Application.Features.User.Command.CreateUser
 {
@@ -8,27 +9,29 @@ namespace Blog.Application.Features.User.Command.CreateUser
     {
         public async Task<CreateUserResult> Handle(CreateUserCommand command, CancellationToken cancellationToken)
         {
-            try
+            //get user from database using user query
+            var matchingUser = await dbContext.Users.FirstOrDefaultAsync(x => x.Email == command.Email);
+
+            //throw not found error if user not found
+            if (matchingUser is not null)
             {
-                //Create User Entity
-                var user = Domain.Models.User.Create(id: UserId.Of(Guid.NewGuid()),
-                                       firstName: command.FirstName,
-                                       lastName: command.LastName,
-                                       email: command.Email,
-                                       password: BCryptEncryptor.HashPassword(command.Password),
-                                       userRole: UserRole.Blogger);
-
-                //Save to database
-                dbContext.Users.Add(user);
-                await dbContext.SaveChangesAsync(cancellationToken);
-
-                //return result
-                return new CreateUserResult(true);
+                throw new BadRequestException($"The User with email '{command.Email}' already exists");
             }
-            catch (Exception ex) {
 
-                throw new InternalServerException("There was something wrong with the server: " + ex.InnerException.Message);
-            }
+            //Create User Entity
+            var user = Domain.Models.User.Create(id: UserId.Of(Guid.NewGuid()),
+                                   firstName: command.FirstName,
+                                   lastName: command.LastName,
+                                   email: command.Email,
+                                   password: BCryptEncryptor.HashPassword(command.Password),
+                                   userRole: UserRole.Blogger);
+
+            //Save to database
+            dbContext.Users.Add(user);
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            //return result
+            return new CreateUserResult(true);
         }
     }
 }
